2fb37d9fa0f714ddf28dc4e3887dc82a94c24665
Leonardo <leosf2222@gmail.com>
2026-02-05 01:04:42 -0800

    made changes on lesson 2 package

--- src/com/neotech/lesson02/FaceBookLogin.java
+++ src/com/neotech/lesson02/FaceBookLogin.java
@@ -8,25 +8,3 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FaceBookLogin {
 
 	public static void main(String[] args) throws InterruptedException {
-
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://facebook.com/");
-		
-		//locate the email textbox
-		
-		//create a WebElement object
-		WebElement emailBox = driver.findElement(By.name("email"));
-		
-		emailBox.sendKeys("neo@tech.com");
-		
-		Thread.sleep(2000);
-		
-		//locate password box
-		
-		WebElement passwordBox = driver.findElement(By.name("pass"));
-		
-		
-		
-	}
-
-}
